9d85974f2d9c3d8351d4700858ab46ebf76d3519
Tanha <nusrattanha897@gmail.com>
2024-01-10 22:43:56 +0600

    string variable declarations and template literals

---
+++ string-intro.js
@@ -0,0 +1,10 @@
+// String is denoted by single quote and double quote.
+const name = 'Nusrat';
+const repoCount = 10;
+
+// concat
+// ?console.log('Hello my name is '+name +' and my repo count is '+ repoCount); 
+
+// 
+// using backticks: increase readability.
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
